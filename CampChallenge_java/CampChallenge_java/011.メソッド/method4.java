ec6fec9514833fae909a5d321722956c014f93df
sige-PC\sige <wellreel@gamil.com>
2017-09-19 15:44:55 +0900

    メソッドの課題を提出します。

---
+++ CampChallenge_java/CampChallenge_java/011.メソッド/method4.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Random;
+
+/**
+ *
+ * @author sige
+ */
+@WebServlet(name = "method4", urlPatterns = {"/method4"})
+public class method4 extends HttpServlet {
+boolean myprofile(PrintWriter pw) {
+    Random rand=new Random();
+    int ran=rand.nextInt(19);
+    if(ran == 0){
+        return false;
+    }
+    pw.print("私の名前は新井滋です。<br>");
+    pw.print("26歳です。<br>");
+    pw.print("趣味は小説を読むことです。<br>");
+    return true;
+}
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet method4</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet method4 at " + request.getContextPath() + "</h1>");
+            for(int i=0;i<10;i++){
+                int count=i+1;
+                out.println("<br>"+ count + "回目<br>");
+            if(myprofile(out)){
+                out.print("正しく実行できました。");
+            }else{
+                out.print("正しく実行できませんでした。");
+            }
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
